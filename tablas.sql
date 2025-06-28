03d98a74a01a3988bdb0641b6480c6d0f049ef98
Cesar <cesarluis4056@gmail.com>
2025-06-28 15:26:41 -0500

    nuevos cambios

--- tablas.sql
+++ tablas.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS users(
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) DEFAULT 'Usuario';
+    recovery_code VARCHAR(6),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
